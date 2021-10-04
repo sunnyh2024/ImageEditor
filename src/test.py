130a3939a2e548db4e03e56c98c45c4a98d0878c
akshayd2020 <63755431+akshayd2020@users.noreply.github.com>
2021-10-04 18:31:39 -0400

    Update test.py

--- src/test.py
+++ src/test.py
@@ -4,14 +4,14 @@ import seamCarver as sc
 import numpy as np
 import random as rand
 
-img = Image.open(r"C:\users\sunny\OneDrive\Desktop\ImageEditor\test.png")
+img = Image.open("test.png")
 seam = sc.findLowestSeam(np.array(img), 0)
 print(seam)
 img = sc.carveSeam(img, 2, 3)
 # seam = sc.findLowestSeam(img, True)
 # print(seam)
 # img = sc.remove_seam(img, seam, 1)
-img.save(r"C:\users\sunny\OneDrive\Desktop\ImageEditor\test2.png")
+img.save("test2.png")
 print(img.size)
 
 # img = img.resize((300, 300))
